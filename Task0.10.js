4bf990eab700ad172246d11c3a0882998fb92e61
Kelebone Teboho Motsau <KeleboneTeboho@gmail.com>
2021-05-04 15:03:24 +0200

    added print, removed duplicates, seperate commas

--- Task0.10.js
+++ Task0.10.js
@@ -1,28 +1,30 @@
 function getCommon(str1, str2) {
-    let common = 'Common Letters: ';
-    let comString1 = '';
-    let comString2 = '';
-    
-    if (str1.length > str2.length)
+  var common = '';  
+  let comString1 = ' ';
+  let comString2 = ' ';
+  
+  if (str1.length > str2.length)
+  {
+      comString1 = str1;
+      comString2 = str2;
+  }
+  else
+  {
+      comString1 = str2;
+      comString2 = str1;
+  }
+  
+  
+  for(var i = 0; i < str1.length ; i++)
     {
-        comString1 = str1;
-        comString2 = str2;
-    }
-    else
-    {
-        comString1 = str2;
-        comString2 = str1;
-    }
-    
-    
-    for(var i = 0; i < str1.length ; i++)
+      if (comString1.indexOf(comString2[i]) !== -1)
       {
-        if (comString1.indexOf(comString2[i]) !== -1)
-        {
-          common += comString2[i];
-        }
-       
+        common += comString2[i];
       }
-      return common;
+     
     }
-    
+    return "Common letters:" + [...new Set(common)];
+  }
+  
+//insert string to print out vowels in the bracket below:
+console.log(getCommon()); 
--- Task0.9.js
+++ Task0.9.js
@@ -1,15 +1,18 @@
-
 function getVowel(str) {
-    let vowelList = 'AEIOUaeiou'
-    let vowels = 'Vowels: ';
-    
-     for(var i = 0; i < str.length ; i++)
+  let vowelList = 'AEIOUaeiou'
+  let vowels = '';
+  
+   for(var i = 0; i < str.length ; i++)
+    {
+      if (vowelList.indexOf(str[i]) !== -1)
       {
-        if (vowelList.indexOf(str[i]) !== -1)
-        {
-          vowels += str[i];
-        }
+        vowels += str[i];
       }
-      return vowels;
+      
     }
+    return 'Vowels: ' + [...new Set(vowels)];
+  }
+
+//insert string to print out vowels in the bracket below:
+console.log(getVowel());  
    
